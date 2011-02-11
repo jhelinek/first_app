1095f32c72e6ff08c930544da43b2bafda2f9315
Your Name <jonathan@jonathan-Studio-XPS-1340.(none)>
2011-02-10 22:05:23 -0500

    done with demo app even though I can't see it due to rails server command not working

---
+++ generate/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_generate_session',
+  :secret      => '2f8f7bd455ccea01cd4d75625929240c5984526c1f34bc518c44343982d0d1680ddffdc5fbeb333658ea6507d74db3e481639014004a33051df16ea48f477556'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
